e5cf2e94a7e6becfa3259f737d4685edbd1acf9d
John DeWolf <jdewolf@backupify.com>
2015-07-11 13:41:04 -0400

    basic drag and drop functioning

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
-'use strict';
+
 
 var rows = document.getElementsByClassName("row");
 
-function addChecker(tile, color) {
+function addChecker(tile, color, id) {
   var checker = document.createElement('div');
   checker.className = "checker checker--" + color;
+  checker.draggable = "true";
+  checker.id = id;
   tile.appendChild(checker);
 }
 
@@ -21,35 +23,101 @@ var blueTeam = [];
 for (var x = 0; x < rows.length; x++) {
   var tiles = rows[x].getElementsByClassName('tile');
   for (var y = 0; y < tiles.length; y++) {
+    var redCounter = 0;
+    var blueCounter = 0;
     if (x < 3) {
-      var counter = 0;
       if (x % 2 === 0 && y % 2 === 0) {
-        addChecker(tiles[y], "red");
-        var redChecker = new CheckerPiece(counter, "red", [x, y]);
+        var id = tiles[y].attributes.x.value.toString() + tiles[y].attributes.y.value.toString();
+        addChecker(tiles[y], "red", id);
+        var redChecker = new CheckerPiece(redCounter, "red", [x, y]);
         redTeam.push(redChecker);
-        counter++;
+        redCounter++;
       }
       else if (x % 2 != 0 && y % 2 !== 0) {
-        addChecker(tiles[y], "red");
-        var redChecker = new CheckerPiece(counter, "red", [x, y]);
+        var id = tiles[y].attributes.x.value.toString() + tiles[y].attributes.y.value.toString();
+        addChecker(tiles[y], "red", id);
+        var redChecker = new CheckerPiece(redCounter, "red", [x, y]);
         redTeam.push(redChecker);
-        counter++;
+        redCounter++;
       }
     }
     else if (x > 4) {
-      var counter = 0;
       if (x % 2 != 0 && y % 2 != 0) {
-        addChecker(tiles[y], "blue");
-        var blueChecker = new CheckerPiece(counter, "blue", [x, y]);
+        var id = tiles[y].attributes.x.value.toString() + tiles[y].attributes.y.value.toString();
+        addChecker(tiles[y], "blue", id);
+        var blueChecker = new CheckerPiece(blueCounter, "blue", [x, y]);
         blueTeam.push(blueChecker);
-        counter++;
+        blueCounter++;
       }
       else if (x % 2 == 0 && y % 2 == 0 ) {
-        addChecker(tiles[y], "blue");
-        var blueChecker = new CheckerPiece(counter, "blue", [x, y]);
+        var id = tiles[y].attributes.x.value.toString() + tiles[y].attributes.y.value.toString();
+        addChecker(tiles[y], "blue", id);
+        var blueChecker = new CheckerPiece(blueCounter, "blue", [x, y]);
         blueTeam.push(blueChecker);
-        counter++;
+        blueCounter++;
       }
     }
   }
 }
+
+document.addEventListener("dragstart", function( event ) {
+    // store a ref. on the dragged elem
+    dragged = event.target;
+}, false);
+
+
+document.addEventListener("dragover", function( event ) {
+    // prevent default to allow drop
+    event.preventDefault();
+}, false);
+
+
+//If tile is playable highlight color to green
+document.addEventListener("dragenter", function( event ) {
+    // highlight potential drop target when the draggable element enters it
+    if ( checkDropLocation(event) != false ) {
+        event.stopPropagation();
+        event.target.style.background = "green";
+    } else {
+      event.target.style.background = 'pink';
+    }
+}, false);
+
+
+//reset after hightlight
+document.addEventListener("dragleave", function( event ) {
+    // reset background of potential drop target when the draggable element leaves it
+    if ( event.target.classList[0] == "tile" ) {
+        event.target.style.background = "";
+    }
+}, false);
+
+document.addEventListener("drop", function( event ) {
+    // prevent default action (open as link for some elements)
+    event.preventDefault();
+    // move dragged elem to the selected drop target
+    if ( checkDropLocation(event) != false && dragged.parentNode != event.target && dragged != event.target  ) {
+        event.target.style.background = "";
+        dragged.parentNode.removeChild( dragged );
+        event.target.appendChild( dragged );
+        if (dragged.classList[1] == 'checker--red') {
+          dragged.style.background = "red";
+        }
+        else {
+          dragged.style.background = 'blue';
+        }
+    }
+
+}, false);
+
+function checkDropLocation (dropLocation) {
+  if ( dropLocation.target.classList[0] == "tile" && dropLocation.target.childElementCount > 0 ) {
+    return false
+  }
+}
+
+//Logic:
+//-team turn
+//-who starts?
+//-where can you drop a checker?
+//if you drop piece where you picked it up it disappears --done
